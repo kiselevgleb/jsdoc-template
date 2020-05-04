3547810e10641b30855e8d23df94e4370c5e1a52
kiselevgleb <kiselevgleb@gmail.com>
2020-05-04 20:17:18 +0300

    added jsdoc

---
+++ src/js/character.js
@@ -0,0 +1,12 @@
+/**
+ * <Общее описание>
+ * 
+ * @param name имя пользователя  
+ * @param type тип пользователя
+ * 
+ * @throws Will throw an error if the argument is null or name.leght size smoller then 2
+ */
+function Character(name, type) {
+  this.name = name;
+  this.type = type;
+}
